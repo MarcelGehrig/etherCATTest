c906689fe5365c2f35042d887dd39a635d2199e4
MarcelGehrig <marcel.gehrig@ntb.ch>
2019-03-12 15:26:38 +0100

    main_eeros example compiles

--- main_eeros.cpp
+++ main_eeros.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <signal.h>
 
-#include <eeros/logger/Logger.hpp>
-#include <eeros/logger/StreamLogWriter.hpp>
-// #include <eeros/safety/SafetySystem.hpp>
 #include <eeros/core/Executor.hpp>
+#include <eeros/safety/SafetySystem.hpp>
+#include <eeros/logger/StreamLogWriter.hpp>
+#include <eeros/sequencer/Sequencer.hpp>
+#include <eeros/hal/HAL.hpp>
+#include "MySafetyProperties.hpp"
+#include "MyControlSystem.hpp"
+#include "MainSequence.hpp"
 
 #include <EtherCATMain.hpp>
 
 #include "etherCatInterface/EtherCATInterfaceElmo.hpp"
+
 using namespace etherCATInterface;
 using namespace eeros::control;
-// using namespace eeros::safety;
+using namespace eeros::safety;
+using namespace eeros::logger;
+using namespace eeros::sequencer;
+using namespace eeros::hal;
 
 
 bool running = true;
@@ -20,68 +28,11 @@ static constexpr int byteSizePerSlave = 32;
 static constexpr int numberOfDrivesTotal = 2;
 static constexpr double dt = 0.001;
 
-
-class ControlSystem;
-// class SafetyPropertiesTest : public SafetyProperties {
-// public:
-// 	SafetyPropertiesTest(double ts);
-// 	
-// 	ControlSystem* controlSystem;
-// 
-// 	SafetyEvent seElmosInitialized;
-// 	
-// 	SafetyLevel slInitializingElmos;
-// 	SafetyLevel slElmosInitialized;
-// };
-
-class ControlSystem {
-public:
-// 	ControlSystem(ecmasterlib::EtherCATMain* etherCATStack, double dt, EtherCATInterfaceElmo* elmoDrives) {
-	ControlSystem(ecmasterlib::EtherCATMain* etherCATStack, double dt, EtherCATInterfaceElmo* elmoDrives) {
-// 		i.getOut().getSignal().setName("integrator output");
-// 		i.getIn().connect(c.getOut());
-// 		checker.setName("check integrator level");
-// 		checker.getIn().connect(i.getOut());
-	}
-
-// 	Constant<> c;
-// 	I<> i;
-// 	SignalChecker<> checker;
-};
-
-// SafetyPropertiesTest::SafetyPropertiesTest(double ts) : 
-// 	seStartRampingUp("start ramping up"),
-// 	seReset("reset"),
-// 	slStart("start"),
-// 	slRampingUp("ramping up")
-// 	{	
-// 	
-// 	// ############ Add levels ############
-// 	addLevel(slStart);
-// 	addLevel(slRampingUp);
-// 	
-// 	// ############ Add events to the levels ############
-// 	slStart.addEvent(seStartRampingUp, slRampingUp, kPrivateEvent);
-// 	slRampingUp.addEvent(seReset, slStart, kPublicEvent);
-// 
-// 	// Define and add level functions
-// 	slStart.setLevelAction([this,ts](SafetyContext* privateContext) {
-// 		controlSystem->i.disable();
-// 		controlSystem->i.setInitCondition(0);
-// 		if(slStart.getNofActivations() * ts > 3) {
-// 			privateContext->triggerEvent(seStartRampingUp);
-// 			controlSystem->checker.reset();
-// 			controlSystem->i.enable();
-// 		}
-// 	});
-// 
-// 	// Define entry level
-// 	setEntryLevel(slStart);	
-// }
-
 static void signalHandler( int nSignal )
 {
     std::cout << "signalHandler" << std::endl;
+	SafetySystem::exitHandler();
+	Sequencer::instance().abort();
     etherCATStackPtr->stop();
     running = false;
     std::cout << "signalHandler" << std::endl;
@@ -91,9 +42,8 @@ static void signalHandler( int nSignal )
 int main(int argc, char **argv) {
 	signal(SIGINT, signalHandler);
 
-	using namespace eeros::logger;
-
-
+	// Logger
+	// ////////////////////////////////////////////////////////////////////////
 	StreamLogWriter w(std::cout);
 	Logger log;
 	log.set(w);
@@ -101,34 +51,52 @@ int main(int argc, char **argv) {
 	log.info() << "Hello, EEROS";
 
 
-
-
 	// EtherCAT
 	// ////////////////////////////////////////////////////////////////////////
 	ecmasterlib::EtherCATMain* etherCATStack = ecmasterlib::EtherCATMain::createInstance(argc, argv, byteSizePerSlave*numberOfDrivesTotal);
 	signal(SIGINT, signalHandler);
-	//   etherCATStackPtr = etherCATStack;
 	sleep(9);
 	EtherCATInterfaceElmo elmoDrives = EtherCATInterfaceElmo( etherCATStack );
 	bool allDrivesAreSwitchedOn = false;
 	while (!allDrivesAreSwitchedOn) {
-		allDrivesAreSwitchedOn = switchOnAllDrives();
+		allDrivesAreSwitchedOn = elmoDrives.switchOnAllDrives();
 		usleep(1e6);	
 	}
 	//   signal(SIGINT, signalHandler);  
 	
+	// HAL
+	// ////////////////////////////////////////////////////////////////////////
+	HAL& hal = HAL::instance();
+	hal.readConfigFromFile(&argc, argv);
+	
 	// Control system
 	// ////////////////////////////////////////////////////////////////////////
-	ControlSystem* controlSystem;
-// 	controlSystem = new ControlSystem(etherCATStack, dt, elmoDrives);
-	controlSystem = new ControlSystem(etherCATStack, dt, &elmoDrives);
+	MyControlSystem controlSys(dt, etherCATStack, &elmoDrives);
+	
+	// Safety system
+	// ////////////////////////////////////////////////////////////////////////
+	MySafetyProperties properties(controlSys, dt);
+	SafetySystem safetySys(properties, dt);
+	controlSys.timedomain.registerSafetyEvent(safetySys, properties.doEmergency);
+	
+	// Sequencer
+	// ////////////////////////////////////////////////////////////////////////
+	auto& sequencer = Sequencer::instance();
+	MainSequence mainSequence("Main Sequence", sequencer, safetySys, properties, controlSys, 3.14/10);
+	sequencer.addSequence(mainSequence);
+	mainSequence.start();
 	
 	
 	// Executor
 	// ////////////////////////////////////////////////////////////////////////
 	auto &executor = eeros::Executor::instance();
 	executor.syncWithEtherCATSTack(etherCATStack);
-// 	executor.setMainTask(safetySystem);
+	executor.setMainTask(safetySys);
+	safetySys.triggerEvent(properties.doSystemOn);
+	
+	executor.run();
+	
+	mainSequence.waitAndTerminate();
 	
 	
 	
